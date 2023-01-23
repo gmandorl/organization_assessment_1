1e3e00829e7a845d7ed8aedb3c7c3a77262f83fd
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2023-01-23 08:45:10 +0100

    fixed ABCOP=inf

--- data_production/organization/run_metrics.py
+++ data_production/organization/run_metrics.py
@@ -18,19 +18,22 @@ def run_metrics (image) :
     #plt.imshow(objects.labeled)
     #plt.show()
 
+    image_size = image.size
+
     properties = dict()
-    properties['area_skm']  = objects.area_skm
+    properties['area']      = objects.area_skm
     properties['area_spg']  = objects.area_spg
     properties['number']    = objects.number_of_objects
-    properties['SCAI']      = metrics.SCAI(pairs_of_objects, image_size=image.size)
-    properties['MCAI']      = metrics.MCAI(pairs_of_objects, image_size=image.size)
+    properties['mean_area'] = properties['area'] / properties['number']
+    properties['SCAI']      = metrics.SCAI(pairs_of_objects, image_size=image_size)
+    properties['MCAI']      = metrics.MCAI(pairs_of_objects, image_size=image_size)
     properties['COP']       = metrics.COP(pairs_of_objects)
-    properties['ABCOP']     = metrics.ABCOP(pairs_of_objects, image_size=image.size)
+    properties['ABCOP']     = metrics.ABCOP(pairs_of_objects, image_size=image_size)
     properties['ROME']      = metrics.ROME(pairs_of_objects)
-    properties['ROME_norm'] = properties['ROME'] / properties['area_skm'] * properties['number']
-    properties['Iorg']      = metrics.Iorg(pairs_of_objects, image_size=image.size)
-    #properties['Iorg2']     = metrics.Iorg2(pairs_of_objects, image_size=image.size)
-    properties['MICA']      = metrics.MICA(pairs_of_objects, image_size=image.size)
+    properties['ROME_norm'] = properties['ROME'] / properties['area'] * properties['number']
+    properties['Iorg']      = metrics.Iorg(pairs_of_objects, image_size=image_size)
+    #properties['Iorg2']     = metrics.Iorg2(pairs_of_objects, image_size=image_size)
+    properties['MICA']      = metrics.MICA(pairs_of_objects, image_size=image_size)
 
     return properties
 
