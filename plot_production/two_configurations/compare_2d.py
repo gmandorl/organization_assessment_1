5bb71be124119ef15e34740fedee312b1f8eea89
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2023-01-27 22:09:09 +0100

    compare 'base' and 'new' for each properties and for all

---
+++ plot_production/two_configurations/compare_2d.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import datetime
+import os
+
+
+
+def draw_and_save ( METRIC,
+                    H2,
+                    axis_label_original,
+                    axis_label_modified,
+                    folder_out = "figure/2d_comparison/",
+                    set_log = False
+                    ) :
+
+    fig, ax   = plt.subplots( figsize=(8,6) )
+
+    # transform the histogram into density
+    SF         =  100 / np.sum(H2[0]) / (H2[1][1] - H2[1][0])**2
+    h2_to_plot =  H2[0]
+    h2_to_plot =  h2_to_plot * SF
+
+
+    # x-y limits
+    xedges  = ( H2[1][:-1] + H2[1][1:] ) / 2.
+    yedges  = ( H2[2][:-1] + H2[2][1:] ) / 2.
+
+    # z limits
+    levels  = np.linspace (SF, h2_to_plot.max(), 11)
+    ticks = np.array([levels[0], levels[2], levels[4], levels[6], levels[8], levels[10]])
+    ticks_round = np.around(ticks if not set_log else np.exp(ticks), 1)
+
+    # plot the density
+    h2_to_plot = np.where(h2_to_plot>levels[0], h2_to_plot, np.nan)
+    contourf_  = plt.contourf(xedges, yedges, h2_to_plot, levels=levels, cmap='Blues')
+
+    # plot the diagonal
+    range_min = xedges[1]
+    range_max = xedges[-2]
+    plt.plot( (range_min,range_max),(range_min,range_max), range_max, color='r', linestyle='--', linewidth=2)
+
+    # add colorbar
+    fig.subplots_adjust(left=0.15, right=0.82)  # add a new axix to the right
+    cbar_ax = fig.add_axes([0.84, 0.2, 0.01, 0.6])
+    fig.colorbar(contourf_, cax=cbar_ax)
+
+    # axis label
+    ax.set_xlabel(axis_label_original)
+    ax.set_ylabel(axis_label_modified)
+    plt.text(1.04, 0.97, 'Density', weight='bold', transform = ax.transAxes)
+
+    plt.savefig(f'{folder_out}/{METRIC}.png')
+
+
+
+
+
+
+def compare_2d( METRIC,
+                df_original,
+                df_modified,
+                axis_label_original,
+                axis_label_modified,
+                nbins      = 50,
+                folder_out = "figure/2d_comparison/"
+                ) :
+
+    original = df_original[METRIC]
+    modified = df_modified[METRIC]
+
+    # select where there are non NAN values
+    non_NAN = np.logical_and( np.isfinite(original),
+                              np.isfinite(modified))
+
+    original = original[non_NAN].to_numpy()
+    modified = modified[non_NAN].to_numpy()
+
+
+    # numpy to plot
+    up_limit   =  np.quantile(original, q=0.99)
+    lo_limit   =  np.quantile(original, q=0.01)
+    plot_range =  [[lo_limit, up_limit], [lo_limit, up_limit]]
+    H2 = np.histogram2d(modified, original, bins=nbins, range=plot_range)
+
+
+
+    if not os.path.isdir(folder_out) : os.makedirs(folder_out)
+
+    # plt font
+    font = {'family' : 'serif','size': 18}
+    plt.rc('font', **font)
+
+
+    draw_and_save( METRIC, H2, axis_label_original, axis_label_modified, folder_out, set_log = False)
+    # log option still not implemented
+    #draw_and_save( METRIC, H2, axis_label_original, axis_label_modified, folder_out, set_log = True )
+
+
+
+
+
+
---
+++ plot_production/two_configurations/variation_all.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import datetime
+import pandas as pd
+import os
+
+
+
+def draw_and_save (METRICS,
+                   df_difference,
+                   y_min = 0.09,
+                   y_max = 99,
+                   x_max = 1.4,
+                   folder_out = 'figure',
+                   extra_text = '',
+                   extra_name = '',
+                   draw_abs = False
+                   ) :
+
+    fig, ax   = plt.subplots( figsize=(8,6) )
+
+    nbins = 100
+    x_min = 0 if draw_abs else -x_max
+    bsize = (x_max-x_min)/nbins
+
+    for METRIC in METRICS :
+        Z_metric    = df_difference[METRIC].to_numpy()
+        sample_size = Z_metric.size
+        if draw_abs : Z_metric = np.abs(Z_metric)
+
+        h1 = np.histogram(Z_metric, bins=nbins, range=(x_min,x_max))#, density=True)
+
+        xbins = (h1[1][1:] + h1[1][:-1] ) /2.
+        density_to_plot = h1[0] / bsize / sample_size  # this normalization is better than numpy's
+        plt.plot(xbins, density_to_plot)#, label=METRIC)
+        plt.fill_between(xbins, y_min, density_to_plot, interpolate=True, alpha=0.25, label=METRIC)
+
+    # set axis
+    ax.spines[['right', 'top', 'left']].set_visible(False)
+    ax.set_xlabel('|Z|' if draw_abs else 'Z')
+    ax.set_ylabel('density')
+    plt.yscale('log')
+    plt.grid(True, axis='y', linestyle='--')
+    plt.tick_params(axis='y', left=False, bottom=False, labelbottom=False,which='both', top=False)
+    plt.ylim([y_min, y_max])
+
+    # vertical line at 0
+    if not draw_abs : plt.axvline(0, color='k', linewidth=1, linestyle='--')
+
+    # legend
+    leg = plt.legend(title='Indices', loc = 'upper right', fontsize=13)
+    plt.setp(leg.get_title(),fontweight='bold')
+    leg.get_frame().set_linewidth(0.0)
+    leg.get_frame().set_color('white')
+    #leg.get_frame().set_alpha(1.)
+
+    plt.savefig(f'{folder_out}/density_Z{extra_name}.png')
+
+    fig.clf()
+
+
+
+def variation_all( METRICS,
+                   df_original,
+                   df_modified,
+                   folder_out = 'figure',
+                   extra_text = ''
+                   ) :
+
+
+
+    # plt font
+    font = {'family' : 'serif','size': 18}
+    plt.rc('font', **font)
+
+    df_original   = df_original[METRICS]
+    df_modified   = df_modified[METRICS]
+    df_difference = pd.DataFrame(index=range(len(df_original)),columns=METRICS)
+
+
+    std = df_original.std(skipna=True)
+    #print(df_original, '\n', std, std['ABCOP'])
+
+    for METRIC in METRICS :
+        df_difference[METRIC] = (df_modified[METRIC] - df_original[METRIC]) / std[METRIC]
+        Z_metric = df_difference[METRIC].to_numpy()
+        print(METRIC, '    \t ', np.nanmean(np.abs(Z_metric)))
+
+
+    if not os.path.isdir(folder_out) : os.makedirs(folder_out)
+
+    draw_and_save (METRICS,
+                   df_difference,
+                   y_min = 0.001,
+                   y_max = 99,
+                   x_max = 5,
+                   folder_out = folder_out,
+                   extra_text = extra_text,
+                   extra_name = '',
+                   draw_abs = False
+                   )
+
+    draw_and_save (METRICS,
+                   df_difference,
+                   y_min = 0.11,
+                   y_max = 99,
+                   x_max = 1.3,
+                   folder_out = folder_out,
+                   extra_text = extra_text,
+                   extra_name = '_zoom',
+                   draw_abs = False
+                   )
+
+    draw_and_save (METRICS,
+                   df_difference,
+                   y_min = 0.011,
+                   y_max = 99,
+                   x_max = 2,
+                   folder_out = folder_out,
+                   extra_text = extra_text,
+                   extra_name = '_abs',
+                   draw_abs = True
+                   )
