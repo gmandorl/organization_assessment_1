c0cfed806825b2a776e40f7f3c2e84c221b60c51
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2023-03-08 18:25:12 +0100

    scale dimensions

--- data_production/organization/merge_csv.py
+++ data_production/organization/merge_csv.py
@@ -13,6 +13,26 @@ parser.add_argument("-d", "--dataset", default='TOOCAN', help="folder where to m
 args = parser.parse_args()
 
 
+factors_F1 = {f'reso{n}': n for n in range(2,7)}
+factors_F3 = {f'smaller{n}': 100./(100.-n) for n in range(1,50)}
+factors = {**factors_F1, **factors_F3}
+
+
+def scale_with_dimension(df, label) :
+
+    if label in factors.keys() :
+        scale_factor = factors[label]
+        for METRIC in df.columns :
+            if   METRIC in ['ROME'] or 'area' in METRIC :          df[METRIC] = df[METRIC] * scale_factor**2
+            elif METRIC in ['NN_center', 'NN_edge'] :              df[METRIC] = df[METRIC] * scale_factor
+            elif METRIC in ['MCAI'] :                              df[METRIC] = df[METRIC] / scale_factor
+            elif METRIC in ['SCAI'] :                              df[METRIC] = df[METRIC] / scale_factor**2
+
+
+    print(label)
+    return df
+
+
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
 
@@ -28,6 +48,8 @@ if __name__ == '__main__':
 
         df = pd.concat((pd.read_csv(f) for f in file_names), ignore_index=True)
         df = df.sort_values(by = ['year', 'month', 'day', 'hour', 'minute'], ascending = True)
+
+        df = scale_with_dimension(df, label)
 
         folder_out = f'output/merged/{args.dataset}/{args.pFolder}/'
         fname = os.listdir(f'{path}{label}/')[0].split('___')[0]
