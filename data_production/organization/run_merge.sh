3a6f5c224b917d2fd5403bd0d74e11645aabddc1
gmandorl <giulio.mandorli@lmd.ipsl.fr>
2023-08-23 13:54:11 +0200

    added dataset name as input

--- data_production/organization/run_merge.sh
+++ data_production/organization/run_merge.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-python merge_csv.py -p P1
-python merge_csv.py -p P2
-python merge_csv.py -p P3
-python merge_csv.py -p P4
-python merge_csv.py -p P9
-python merge_csv.py -p F1
-python merge_csv.py -p F3
-python merge_csv.py -p F4
+dataset=$1
+
+python merge_csv.py -p P1 -d $dataset
+python merge_csv.py -p P2 -d $dataset
+python merge_csv.py -p P3 -d $dataset
+# python merge_csv.py -p P4 -d $dataset
+python merge_csv.py -p P5 -d $dataset
+python merge_csv.py -p P8 -d $dataset
+python merge_csv.py -p P9 -d $dataset
+python merge_csv.py -p F1 -d $dataset
+python merge_csv.py -p F3 -d $dataset
+python merge_csv.py -p F4 -d $dataset
